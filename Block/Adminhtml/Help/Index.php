d20f4833c1b435702d2e7527edc2eb55ef1c5816
Dominic Watts <dominic@xigen.co.uk>
2020-05-19 01:19:24 +0100

    1.0.2 release, snag/feature list

---
+++ Block/Adminhtml/Help/Index.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+
+namespace Xigen\Announce\Block\Adminhtml\Help;
+
+
+class Index extends \Magento\Backend\Block\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
+
